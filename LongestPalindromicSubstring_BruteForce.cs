f24950bf2c76c51359282adb11a03b7fdfe4d460
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2021-05-03 00:48:28 +0530

    Update LongestPalindromicSubstring_BruteForce.cs

--- LongestPalindromicSubstring_BruteForce.cs
+++ LongestPalindromicSubstring_BruteForce.cs
@@ -1,36 +1,31 @@
 public string LongestPalindrome(string s) {
         if(s == null || s.Length == 0)
             return null;
-       string result = s[0].ToString();
+        
+        string result = s[0].ToString();
         
         for(int i = 0; i < s.Length; i++)
         {
-            int l = i - 1;
-            int r = i + 1;
-            
-            //for odd
-            while(l >= 0 && r <= s.Length - 1 && s[l] == s[r])
-            {
-                string subString = s.Substring(l, r-l + 1);
-                if(subString.Length > result.Length)
-                    result = subString;
-                l--;
-                r++;
-            }
-            
-            //for even
-            l = i;
-            r = i+1;
-            
-            while(l >= 0 && r <= s.Length - 1 && s[l] == s[r])
-            {
-                string subString = s.Substring(l, r-l + 1);
-                if(subString.Length > result.Length)
-                    result = subString;
-                l--;
-                r++;
-            }
+               for(int j = i + 1; j < s.Length; j++)
+               {
+                   string sub = s.Substring(i, j - i + 1);
+                   if(IsPalindrome(sub) && sub.Length > result.Length)
+                   {
+                       result = sub;
+                   }
+               }
         }
         
         return result;
+    }
+    
+    private bool IsPalindrome(string s)
+    {
+        for(int i = 0, j = s.Length -1; i<=j; i++,j--)
+        {
+            if(s[i] != s[j])
+                return false;
+        }
+        
+        return true;
     }
